4257a1ae0cf63e76c68a02aaf2db3516aae59ff5
Ubuntu <ubuntu@ip-172-31-51-232.ec2.internal>
2021-09-17 17:24:01 +0000

    added file

---
+++ constants.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# Created by: Seti Ngabo
+# Created on: Sept 2021
+# This module contains constants for the pizza program
+THE_NUMBER = 5
---
+++ guessing_number.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# Created by: Seti Ngabo
+# Created on: Sept 2021
+# This program guesses a random number
+
+import constants
+
+
+def main():
+    # this function checks if the random number is correct
+    
+    #input
+    guess = int(input("Enter the number between 0 and 9: "))
+    print("")
+    
+    # process and output
+    if guess == constants.THE_NUMBER:
+        print("Correct")
+        print("")
+        print("Done.")
+    else:
+        print("wrong, try again.")
+        
+
+if __name__ == "__main__":
+    main()
